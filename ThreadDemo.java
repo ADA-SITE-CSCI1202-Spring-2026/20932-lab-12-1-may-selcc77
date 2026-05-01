32369576785e4977ee93ef4cce24013ae32f0db9
Seljan Abbasova <sabbasova23925@ada.edu.az>
2026-05-01 10:55:01 +0400

    task06

---
+++ ThreadDemo.java
@@ -0,0 +1,16 @@
+public class ThreadDemo {
+    public static void main(String[] args) {
+
+        // ===== TODO 1 =====
+        Thread t = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i);
+            }
+        });
+
+        // ===== TODO 2 =====
+        t.start();
+
+        System.out.println("Main method finished.");
+    }
+}
